4d4318d8266509f6e134f81d1b1b896af9aa29be
Maaz Junior <goalmaaz@gmail.com>
2022-07-17 00:05:38 +0100

    i added home page to my project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Pages from './pages/pages';
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 const App = () => {
 
   return(
+    <BrowserRouter>
     <div>
-      <h1>HELLO WORLD</h1> 
+      <Pages />
     </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+
+  return(
+    <div className='home'>
+        <Navbar />
+        <div className='immersive'>
+            <h2>IMMERSIVE EXPERIENCE THAT DELIVER</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Home;
