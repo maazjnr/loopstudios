860fa6c821bf509efa1a2f9e363b0306432b9005
Maaz Junior <goalmaaz@gmail.com>
2022-07-18 14:58:10 +0100

    fixed the bug

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,13 +12,14 @@ const About = () => {
                 </div> 
 
                 <div className="about-details">
-                    <h1>THE LEADER IN  INTERACTIVE VR</h1>
+                    <h1>THE LEADER IN  INTERACTIVE VR </h1>
                     <h5>
                         Founded in 2011, Loopstudios has been producing
                         world-class virtual reality projects for some
                         of the best companies around the globe. Our award
                         wining creations have transformed businesses through
                         through digital experiences that bind to their brand.
+                        
                     </h5>
                 </div>
             </section>
